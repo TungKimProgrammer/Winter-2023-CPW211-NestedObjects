eb2eeedfc4a7d2db9fee57860201493dfbb72645
TUNG N KIM <Tungkims@gmail.com>
2023-01-13 02:32:59 -0800

    Added some test code

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new Advisor()
+{
+    Email = "JoeOrtiz@cptc.edu",
+    FullName = "Joe Ortiz",
+    OfficeLocation = "B11 Rm 110"
+};
+
+Student stu1 = new Student()
+{
+    FirstName = "Tung",
+    LastName = "Kim",
+    DateOfBirth = new DateOnly(1984, 6, 3),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "8008889999",
+    SchoolEmail = "kim9999@students.cptc.edu"
+};
+
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName} as the computer scient advisor!");
+Console.WriteLine($"Student {stu1.FirstName}'s advisor, {stu1.AssignedAdvisor.FullName}, can be reached at {stu1.AssignedAdvisor.Email} or {stu1.AssignedAdvisor.OfficeLocation}");
